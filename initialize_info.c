2b71f4e09107995dea437a6fd0d433e28d4ebddb
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-20 01:31:21 +0300

    add initialize_info() function

---
+++ initialize_info.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * initialize_info - initialize info struct
+ * @info: info struct address
+ * @av: argument vector from main()
+ */
+void initialize_info(info_t *info, char **av)
+{	
+	int i = 0;
+	info->file_name = av[0];		
+	if (info->arg)				
+	{		
+		info->argv = split_string(info->arg, " \t");	
+		if (!info->argv)							
+		{
+			info->argv = malloc(sizeof(char *) * 2);		
+			if (info->argv)								
+			{				
+				info->argv[0] = _strdup(info->arg);			
+				info->argv[1] = NULL;								
+			}						
+		}			
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;														alias_replace(info);			
+		variables_substitute(info);
+	}
+}
