dd87dee8c86a720b03c3c7c21fc25841855bfd57
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-20 01:18:16 +0300

    add handle_sigint_signal() function

---
+++ handle_sigint_signal.c
@@ -0,0 +1,13 @@
+#include "shell.c"
+
+/**
+ * handle_sigint_signal - handles SIGINT
+ * @sig_num: signal
+ */
+
+void handle_sigint_signal(__attribute__((unused)) int sig_num)
+{	
+	_puts("\n");
+	_puts("$ ");		
+	_putchar(BUFFER_FLUSH);
+}
