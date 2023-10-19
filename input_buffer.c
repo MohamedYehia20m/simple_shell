aab733ea78926094bb57a8da71d539a9e3444d15
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-20 01:18:50 +0300

    add input_buffer() function

---
+++ input_buffer.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * input_buffer - reads input from stdin
+ * @info: pointer to info struct
+ * @buffer: pointer to char buffer
+ * @len: pointer to length
+ * Return: number of bytes read || -1
+ */
+
+ssize_t input_buffer(info_t *info, char **buffer, size_t *len)
+{
+	ssize_t r = 0;
+	size_t input_len_p = 0;
+	
+	if (!*len)
+	{
+		free(*buffer);
+		*buffer = NULL;
+		signal(SIGINT, handle_sigint_signal);
+		r = _getline(info, buffer, &input_len_p);
+		if (r > 0)	
+		{			
+			if ((*buffer)[r - 1] == '\n')		
+			{						
+				(*buffer)[r - 1] = '\0'; /* remove trailing newline */		
+				r--;										
+			}				
+			info->line_count_flag = 1;			
+			remove_comments(*buffer);						
+			{						
+				*len = r;			
+				info->command_buffer = buffer;					
+			}							
+		}			
+	}		
+	return (r);
+}
