6f5882758b31b70ec0df1dc5cf8e8ee13ba68014
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-20 10:41:02 -0700

    modify _getenv function

--- _getenv.c
+++ _getenv.c
@@ -4,20 +4,19 @@
  * _getenv - Gets the value of an environment variable.
  * @info: Structure containing potential arguments.
  * @name: Name of the environment variable.
+ *
  * Return: environment variable value
  */
-
 char *_getenv(info_t *info, const char *name)
 {
 	list_t *node = info->env;
 	char *ptr;
 
-	while (node)
-	{
+	do {
 		ptr = find_prefix(node->str, name);
 		if (ptr && *ptr)
 			return (ptr);
 		node = node->next;
-	}
+	} while (node);
 	return (NULL);
 }
