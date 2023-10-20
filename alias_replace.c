be045e88c8dad817ae8e23d24a90cc9307a47e07
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-20 13:02:05 -0700

    modify alias_replace

--- alias_replace.c
+++ alias_replace.c
@@ -2,29 +2,30 @@
 
 /**
  * alias_replace - replaces alias
- * @info: the parameter struct
+ * @info: struct info
  *
- * Return: 1 if replaced, 0 otherwise
+ * Return: 1 if replaced
+ *		0 otherwise
  */
 int alias_replace(info_t *info)
 {
-	int i;
-	list_t *node;
+	int ctr = 0;
+	list_t *L_node;
 	char *ptr;
 
-	for (i = 0; i < 10; i++)
-	{
-		node = node_starts_with(info->alias, info->argv[0], '=');
-		if (!node)
+	do {
+		L_node = node_starts_with(info->alias, info->argv[0], '=');
+		if (!L_node)
 			return (0);
 		free(info->argv[0]);
-		ptr = _strchr(node->str, '=');
+		ptr = _strchr(L_node->str, '=');
 		if (!ptr)
 			return (0);
 		ptr = _strdup(ptr + 1);
 		if (!ptr)
 			return (0);
 		info->argv[0] = ptr;
-	}
+		ctr++;
+	} while (ctr < 10);
 	return (1);
 }
