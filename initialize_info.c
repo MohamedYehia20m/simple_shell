be8f92c6b075f9a09deb8f93a40e54b5c76ff5cd
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-20 12:54:35 +0300

    fix initialize_info() betty

--- initialize_info.c
+++ initialize_info.c
@@ -6,24 +6,26 @@
  * @av: argument vector from main()
  */
 void initialize_info(info_t *info, char **av)
-{	
+{
 	int i = 0;
-	info->file_name = av[0];		
-	if (info->arg)				
-	{		
-		info->argv = split_string(info->arg, " \t");	
-		if (!info->argv)							
+
+	info->file_name = av[0];
+	if (info->arg)
+	{
+		info->argv = split_string(info->arg, " \t");
+		if (!info->argv)
 		{
-			info->argv = malloc(sizeof(char *) * 2);		
-			if (info->argv)								
-			{				
-				info->argv[0] = _strdup(info->arg);			
-				info->argv[1] = NULL;								
-			}						
-		}			
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
 		for (i = 0; info->argv && info->argv[i]; i++)
 			;
-		info->argc = i;														alias_replace(info);			
+		info->argc = i;
+		alias_replace(info);
 		variables_substitute(info);
 	}
 }
