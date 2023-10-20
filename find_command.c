db0ca0ffd4beb6d5fc998db6da5681f6c6b2364d
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-20 12:48:36 +0300

    fix find_command() betty

--- find_command.c
+++ find_command.c
@@ -12,30 +12,30 @@ void find_command(info_t *info)
 
 	info->path = info->argv[0];
 	if (info->line_count_flag == 1)
-	{			
-		info->line_count++;	
-		info->line_count_flag = 0;				
-	}					
-	for (index = 0, k = 0; info->arg[index]; index++)	
-		if (!is_delim(info->arg[index], " \t\n"))							
-			k++;						
-	if (!k)			
-		return;	
-	path = search_path(info, _getenv(info, "PATH="), info->argv[0]);		
-	if (path)									
-	{		
-		info->path = path;	
-		fork_command(info);				
-	}				
-	else	
-	{	
-		if ((interactive(info) || _getenv(info, "PATH=")	
+	{
+		info->line_count++;
+		info->line_count_flag = 0;
+	}
+	for (index = 0, k = 0; info->arg[index]; index++)
+		if (!is_delim(info->arg[index], " \t\n"))
+			k++;
+	if (!k)
+		return;
+	path = search_path(info, _getenv(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		fork_command(info);
+	}
+	else
+	{
+		if ((interactive(info) || _getenv(info, "PATH=")
 			|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
 			fork_command(info);
 		else if (*(info->arg) != '\n')
 		{
-			info->status = COMMAND_NOT_FOUND;	
-			print_error(info, "not found\n");	
-		}				
+			info->status = COMMAND_NOT_FOUND;
+			print_error(info, "not found\n");
+		}
 	}
 }
