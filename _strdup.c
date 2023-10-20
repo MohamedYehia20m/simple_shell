cac6098f764e0290c4486221dd9e666d178bc4c5
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-20 13:06:55 -0700

    modify string compare function

--- _strdup.c
+++ _strdup.c
@@ -1,24 +1,30 @@
 #include "shell.h"
 
 /**
- * _strdup - duplicates a string
- * @str: the string to duplicate
+ * _strdup - returns a pointer to a newly allocated space in
+ *		memory, which contains a copy of the string
+ *		given as a parameter.
+ * @str: pointer to the string.
  *
- * Return: pointer to the duplicated string
-*/
-char *_strdup(const char *str)
+ * Return: NULL if str = NULL
+ *	 On success, the _strdup function a pointer to
+ *	 the duplicated string.
+ *	 NULL if insufficient memory was available
+ */
+char *_strdup(char *str)
 {
-	int length = 0;
-	char *result;
+	int length = 0, i;
+	char *copy_str;
 
 	if (str == NULL)
 		return (NULL);
-	while (*str++)
+	/* string length calculation */
+	while (str[length] != '\0')
 		length++;
-	result = malloc(sizeof(char) * (length + 1));
-	if (!result)
+	copy_str = malloc(sizeof(char) * length + 1);
+	if (copy_str == NULL)
 		return (NULL);
-	for (length++; length--;)
-		result[length] = *--str;
-	return (result);
+	for (i = 0; i < length; i++)
+		copy_str[i] = str[i];
+	return (copy_str);
 }
