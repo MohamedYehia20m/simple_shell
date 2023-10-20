ef61b02222d8f0ad40e592db7ace6a2a602c80d2
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-20 13:56:54 -0700

    modify strcmp

--- _strcmp.c
+++ _strcmp.c
@@ -9,15 +9,10 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
-	}
-	if (*s1 == *s2)
-		return (0);
-	else
-		return (*s1 < *s2 ? -1 : 1);
+	int i = 0;
+
+	while (s1[i] == s2[i])
+		if (s1[i++] == '\0')
+			break;
+	return (s1[i] - s2[i]);
 }
