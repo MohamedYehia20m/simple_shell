999606301dd835c26122f9a72ede999b250e9566
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-20 12:43:01 -0700

    modify print_list_str part 2

--- print_list_str.c
+++ print_list_str.c
@@ -2,19 +2,19 @@
 
 /**
  * print_list_str - prints all elements of a list_t linked list
- * @node: pointer to first node in the list
+ * @P_node: pointer to first node in the list
  *
  * Return: number of elements
 */
-size_t print_list_str(const list_t *L_node)
+size_t print_list_str(const list_t *P_node)
 {
 	size_t elemCtr = 0;
 
-	while (L_node)
+	while (P_node)
 	{
-		_puts(L_node->str ? L_node->str : "(nil)");
+		_puts(P_node->str ? P_node->str : "(nil)");
 		_puts("\n");
-		node = L_node->next;
+		P_node = P_node->next;
 		elemCtr++;
 	}
 	return (elemCtr);
