8b4852ba931e2d478a859c927eb6b5278102676d
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-20 13:08:18 -0700

    modify string compare function

--- _strcmp.c
+++ _strcmp.c
@@ -1,23 +1,18 @@
 #include "shell.h"
 
 /**
- * _strcmp - compares two strings
- * @str1: first string
- * @str2: second string
- *
- * Return: 0 if equal, -1 if str1 < str2, 1 if str1 > str2
+* _strcmp - compares two strings.
+* @s1: pointer to the first address of the first string.
+* @s2: pointer to the first address of the second string.
+*
+* Return: 0 if they are equal, +ve if s1 > s2 or -ve if s2 > s1.
 */
-int _strcmp(char *str1, char *str2)
+int _strcmp(char *s1, char *s2)
 {
-	while (*str1 && *str2)
-	{
-		if (*str1 != *str2)
-			return (*str1 - *str2);
-		str1++;
-		str2++;
-	}
-	if (*str1 == *str2)
-		return (0);
-	else
-		return (*str1 < *str2 ? -1 : 1);
+	int i = 0;
+
+	while (s1[i] == s2[i])
+		if (s1[i++] == '\0')
+			break;
+	return (s1[i] - s2[i]);
 }
