f36a86b03db3a9c17a3e71a2e828802ae75108ca
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-20 12:57:27 +0300

    fix read_buffer() betty

--- read_buffer.c
+++ read_buffer.c
@@ -12,10 +12,10 @@ ssize_t read_buffer(info_t *info, char *read_buffer, size_t *read_pos)
 {
 	ssize_t read_result = 0;
 
-	if (*read_pos)	
-		return (0);		
-	read_result = read(info->file_descriptor, read_buffer, READ_BUFFER_SIZE);			
-	if (read_result >= 0)			
-		*read_pos = read_result;			
+	if (*read_pos)
+		return (0);
+	read_result = read(info->file_descriptor, read_buffer, READ_BUFFER_SIZE);
+	if (read_result >= 0)
+		*read_pos = read_result;
 	return (read_result);
 }
