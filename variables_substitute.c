2c6e2b87a168fafd4ea7072963e8997d13881838
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-20 12:21:14 -0700

    modify variables_substitute

--- variables_substitute.c
+++ variables_substitute.c
@@ -9,33 +9,40 @@
 int variables_substitute(info_t *info)
 {
 	int i = 0;
-	list_t *node;
+	list_t *L_node;
 
-	for (i = 0; info->argv[i]; i++)
+	while (info->argv[i])
 	{
 		if (info->argv[i][0] != '$' || !info->argv[i][1])
 			continue;
 
 		if (!_strcmp(info->argv[i], "$?"))
 		{
-			string_replace(&(info->argv[i]),
-						   _strdup(convert_number(info->status, 10, 0)));
+			string_replace(
+					&(info->argv[i]),
+					_strdup(convert_number(info->status, 10, 0))
+					);
 			continue;
 		}
 		if (!_strcmp(info->argv[i], "$$"))
 		{
-			string_replace(&(info->argv[i]),
-						   _strdup(convert_number(getpid(), 10, 0)));
+			string_replace(
+					&(info->argv[i]),
+					_strdup(convert_number(getpid(), 10, 0))
+					);
 			continue;
 		}
-		node = node_starts_with(info->env, &info->argv[i][1], '=');
-		if (node)
+		L_node = node_starts_with(info->env, &info->argv[i][1], '=');
+		if (L_node)
 		{
-			string_replace(&(info->argv[i]),
-						   _strdup(_strchr(node->str, '=') + 1));
+			string_replace(
+					&(info->argv[i]),
+					_strdup(_strchr(L_node->str, '=') + 1)
+					);
 			continue;
 		}
 		string_replace(&info->argv[i], _strdup(""));
+		i++;
 	}
 	return (0);
 }
