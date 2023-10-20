81f3299b84d82981943caed2c331ff326349c22e
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-20 12:56:29 +0300

    fix input_buffer() betty

--- input_buffer.c
+++ input_buffer.c
@@ -12,27 +12,27 @@ ssize_t input_buffer(info_t *info, char **buffer, size_t *len)
 {
 	ssize_t r = 0;
 	size_t input_len_p = 0;
-	
+
 	if (!*len)
 	{
 		free(*buffer);
 		*buffer = NULL;
 		signal(SIGINT, handle_sigint_signal);
 		r = _getline(info, buffer, &input_len_p);
-		if (r > 0)	
-		{			
-			if ((*buffer)[r - 1] == '\n')		
-			{						
-				(*buffer)[r - 1] = '\0'; /* remove trailing newline */		
-				r--;										
-			}				
-			info->line_count_flag = 1;			
-			remove_comments(*buffer);						
-			{						
-				*len = r;			
-				info->command_buffer = buffer;					
-			}							
-		}			
-	}		
+		if (r > 0)
+		{
+			if ((*buffer)[r - 1] == '\n')
+			{
+				(*buffer)[r - 1] = '\0'; /* remove trailing newline */
+				r--;
+			}
+			info->line_count_flag = 1;
+			remove_comments(*buffer);
+			{
+				*len = r;
+				info->command_buffer = buffer;
+			}
+		}
+	}
 	return (r);
 }
