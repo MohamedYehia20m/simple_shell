dbef00a416775a4cdaafd77a5d32c5dc66e4cdbc
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-20 11:27:05 -0700

    modify realloc

--- _realloc.c
+++ _realloc.c
@@ -1,31 +1,44 @@
 #include "shell.h"
 
 /**
- * _realloc - reallocates a block of memory
- * @ptr: pointer to previous allocated block
- * @old_size: byte size of previous block
- * @new_size: byte size of new block
+ * _realloc - reallocates a memory block using malloc and free.
+ * @ptr: a pointer to the memory previously allocated with a call to
+ *	malloc: malloc(old_size).
+ * @old_size: the size, in bytes, of the allocated space for ptr.
+ * @new_size: the new size, in bytes of the new memory block.
  *
- * Return: pointer to new block
-*/
+ * Return: pointer to the new allocted memory.
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_address;
+	char *newPtr;
+	char *oldPtr;
+	unsigned int i;
 
-	if (!ptr)
-		return (malloc(new_size));
-	if (!new_size)
-		return (free(ptr), NULL);
 	if (new_size == old_size)
 		return (ptr);
-
-	new_address = malloc(new_size);
-	if (!new_address)
+	else if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
 		return (NULL);
-
-	old_size = old_size < new_size ? old_size : new_size;
-	while (old_size--)
-		new_address[old_size] = ((char *)ptr)[old_size];
+	}
+	oldPtr = ptr;
+	newPtr = malloc(new_size);
+	if (newPtr == NULL)
+		return (NULL);
+	if (old_size < new_size)
+		for (i = 0; i < old_size; i++)
+			newPtr[i] = oldPtr[i];
+	else
+		for (i = 0; i < new_size; i++)
+			newPtr[i] = oldPtr[i];
 	free(ptr);
-	return (new_address);
+	return (newPtr);
 }
